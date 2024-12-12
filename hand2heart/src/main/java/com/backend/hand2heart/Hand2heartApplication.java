fd726d6c28b687be8d17578a6838531af73938a9
Ghefin Indra <88959081+GhefinIndra@users.noreply.github.com>
2024-12-12 11:48:35 +0700

    Add files via upload

---
+++ hand2heart/src/main/java/com/backend/hand2heart/Hand2heartApplication.java
@@ -0,0 +1,13 @@
+package com.backend.hand2heart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hand2heartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hand2heartApplication.class, args);
+	}
+
+}
---
+++ hand2heart/src/test/java/com/backend/hand2heart/Hand2heartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.hand2heart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hand2heartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
